987206115782aa6b851fccd775ffce99624f3c00
vovilon228 <113108239+vovilon228@users.noreply.github.com>
2023-06-28 12:59:49 +0300

    Update Strassen.h

--- LAB3/Strassen.h
+++ LAB3/Strassen.h
@@ -1,24 +1,13 @@
 #pragma once
 #include <iostream>
 #include "ComplexMatrix.h"
-/// @brief Represents a class for performing matrix multiplication using the Strassen algorithm.
+
 class Strassen {
 public:
-    /// @brief Performs matrix multiplication using the regular algorithm.
-    /// @param a The first input matrix.
-    /// @param b The second input matrix.
-    /// @return Pointer to the resulting matrix.
+
     static ComplexMatrix* regularMult(ComplexMatrix* a, ComplexMatrix* b);
 
-    /// @brief Performs the recursive step of the Strassen algorithm.
-    /// @param a The first input matrix.
-    /// @param b The second input matrix.
-    /// @return Pointer to the resulting matrix.
     static ComplexMatrix* strassenRecursion(ComplexMatrix* a, ComplexMatrix* b);
 
-    /// @brief Performs matrix multiplication using the Strassen algorithm.
-    /// @param a The first input matrix.
-    /// @param b The second input matrix.
-    /// @return Pointer to the resulting matrix.
     static ComplexMatrix* strassenMultiply(ComplexMatrix* a, ComplexMatrix* b);
 };
