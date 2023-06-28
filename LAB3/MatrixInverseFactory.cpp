437ddcdcc8312a84907e180d0eb8f8b5e98686e4
vovilon228 <113108239+vovilon228@users.noreply.github.com>
2023-06-28 12:57:46 +0300

    Update MatrixInverseFactory.cpp

--- LAB3/MatrixInverseFactory.cpp
+++ LAB3/MatrixInverseFactory.cpp
@@ -1,8 +1,4 @@
 #include "MatrixInverseFactory.h"
-/// @brief Calculates the inverse of the given matrix using the specified algorithm.
-/// @param matrix The input matrix for which to calculate the inverse.
-/// @param algorithm The algorithm to be used for the inverse calculation.
-/// @return The calculated inverse matrix.
 ComplexMatrix MatrixInverseFactory::calculateInverse(const ComplexMatrix& matrix, InverseAlgorithm algorithm) {
     switch (algorithm) {
     case InverseAlgorithm::LU:
