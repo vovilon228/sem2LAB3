71da9f0beb93048e741a52e8fac7223abbe848ba
vovilon228 <113108239+vovilon228@users.noreply.github.com>
2023-06-28 13:00:26 +0300

    Update main.cpp

--- LAB3/main.cpp
+++ LAB3/main.cpp
@@ -1,5 +1,4 @@
-﻿// matrix_algorithms.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+
 
 #include <iostream>
 #include <cstdlib>
@@ -17,14 +16,14 @@ namespace lab {
         return context.run();
     }
     int main() {
-        // Generate random matrix
+      
         int matrixSize = 4;
         ComplexMatrix A(matrixSize, matrixSize);
         A.auto_gen(5, 50, 5, 50);
         std::cout << "Random Matrix A:" << std::endl;
         A.print();
 
-        // Choose inverse algorithm
+
         std::cout << "Choose an inverse algorithm: " << std::endl;
         std::cout << "1. LU Inverse" << std::endl;
         std::cout << "2. Parallel LU Inverse" << std::endl;
@@ -73,13 +72,3 @@ namespace lab {
     }
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
